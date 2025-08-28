82b7a0b427957923e4bdc90a0f68723850895947
KaBarbi <1a35548335+KBarbi@users.noreply.github.com>
2025-08-28 18:07:47 -0300

    game routes api

--- games/views.py
+++ games/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Game
+from .serializers import GameSerializer
 
-# Create your views here.
+
+class GameViewSet(viewsets.ModelViewSet):
+    queryset = Game.objects.all().order_by("-created_at")
+    serializer_class = GameSerializer
+
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ["title", "description"]
+    ordering_fields = ["price", "created_at"]

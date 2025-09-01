e8faaffe94ecc5acaf3293f29b1395cc5d3f9893
KaBarbi <1a35548335+KBarbi@users.noreply.github.com>
2025-09-01 11:31:03 -0300

    cart app

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings
+from games.models import Game
 
-# Create your models here.
+
+class Cart(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="carts"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Carrinho de {self.user.username}"
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity}x {self.game.title}"
--- setup/urls.py
+++ setup/urls.py
@@ -2,11 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from games.views import GameViewSet
+from cart.views import CartViewSet, CartItemViewSet
+
 
 router = DefaultRouter()
 router.register(r'games', GameViewSet, basename="game")
+router.register(r'carts', CartViewSet, basename="cart")
+router.register(r'cart-items', CartItemViewSet, basename="cartitem")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include(router.urls)),  # rotas da API
+    path("api/", include(router.urls)),
 ]

c9fb8039baf3e4d4bba7ca1c175ec46c1e172f81
KaBarbi <135548335+KaBarbi@users.noreply.github.com>
2025-11-13 14:26:06 -0300

    [fix]: games serializer

--- games/serializers.py
+++ games/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from .models import Game
 
+
 class GameSerializer(serializers.ModelSerializer):
+    platform_display = serializers.CharField(
+        source='get_platform_display', read_only=True)
+
     class Meta:
         model = Game
-        fields = "__all__"  
+        fields = ['id', 'title', 'description', 'price',
+                  'stock', 'platform', 'platform_display', 'cover']

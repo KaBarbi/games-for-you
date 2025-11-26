d2fb4a732219ec876555586ca542482f991afcd7
KaBarbi <135548335+KaBarbi@users.noreply.github.com>
2025-11-26 09:54:10 -0300

    [refactor]: games serializer

--- games/serializers.py
+++ games/serializers.py
@@ -4,9 +4,19 @@ from .models import Game
 
 class GameSerializer(serializers.ModelSerializer):
     platform_display = serializers.CharField(
-        source='get_platform_display', read_only=True)
+        source='get_platform_display', read_only=True
+    )
+    cover = serializers.SerializerMethodField()
 
     class Meta:
         model = Game
-        fields = ['id', 'title', 'description', 'price',
-                  'stock', 'platform', 'platform_display', 'cover']
+        fields = [
+            'id', 'title', 'description', 'price',
+            'stock', 'platform', 'platform_display', 'cover'
+        ]
+
+    def get_cover(self, obj):
+        # return only the filename of the cover image
+        if obj.cover:
+            return obj.cover.name.split('/')[-1]
+        return None
